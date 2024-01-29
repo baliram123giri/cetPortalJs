021076488fc54475467b9166902bbcbb907f1c6f
baliram123giri <giri710401@gmail.com>
2024-01-29 09:55:46 +0530

    header middle part done

--- src/index.js
+++ src/index.js
@@ -47,16 +47,18 @@ const header = document.createElement("header")
 // if we use append only then element may anything 
 //append the header to body
 
-///css part
-header.style.borderBottom = "1px solid #000"
-root.appendChild(header)
 
+root.appendChild(header)
+const headerWrapper = document.createElement("div")
 //add container to header
 const headerContainer = document.createElement("div")
-headerContainer.className = "container"
+headerContainer.style.width = "80%"
+headerContainer.className = "mx-auto"
+///css part
+headerWrapper.style.borderBottom = "1px solid #000"
 
-
-header.appendChild(headerContainer)
+headerWrapper.appendChild(headerContainer)
+header.appendChild(headerWrapper)
 
 //add navbar to headerContainer
 const headerNav = document.createElement("nav")
@@ -76,38 +78,48 @@ headerNav.appendChild(headerNavUl)
 const array = [
     {
         title: "Skip to main content",
-        icon: "fa-arrow-down"
+        icon: "fa-arrow-down",
+        flex: 1
     },
     {
-        icon: "fa-volume-up"
+        icon: "fa-volume-up",
+        flex: 1
     },
     {
-        title: "A"
+        title: "A",
+        flex: 1
     },
     {
-        icon: "fa-sitemap"
+        icon: "fa-sitemap",
+        flex: 1
     },
     {
-        icon: "fa-search"
+        icon: "fa-search",
+        flex: 1
     },
     {
-        icon: "fa-youtube-play"
+        icon: "fa-youtube-play",
+        flex: 0.3,
     },
     {
-        icon: "fa-facebook-official"
+        icon: "fa-facebook-official",
+        flex: 0.3,
     },
     {
-        icon: "fa-instagram"
+        icon: "fa-instagram",
+        flex: 0.3
     },
     {
         title: "twitter",
-        icon: "fa-twitter"
+        icon: "fa-twitter",
+        flex: 1,
     }
 ]
 
 for (let i = 0; i < array.length; i++) {
     // console.log(array[i].title)
     const headerNavUlLi = document.createElement("li")
+    headerNavUlLi.style.flex = array[i].flex
     //create a tag for li
     const headerAnchorTag = document.createElement("a")
     headerAnchorTag.className = "text-dark text-decoration-none"
@@ -120,7 +132,91 @@ for (let i = 0; i < array.length; i++) {
     headerNavUl.appendChild(headerNavUlLi)
 }
 
+//header middle
+const headerMiddle = document.createElement("div")
+headerMiddle.className = "container"
+const headerMiddlerWrapper = document.createElement("div")
+headerMiddlerWrapper.style.width = "60%"
+headerMiddlerWrapper.className = "d-flex mt-2"
+//tetsing
+headerMiddlerWrapper.style.height = "100px"
 
+
+//create first ele div
+const satyMevJayteLogo = document.createElement("div")
+satyMevJayteLogo.style.width = "100px"
+satyMevJayteLogo.style.height = "100px"
+
+//create a img
+const satyMevJayteLogoImg = document.createElement("img")
+satyMevJayteLogoImg.src = "../img/satyamev-jayte-removebg-preview.png"
+satyMevJayteLogoImg.alt = "satyamev-jayte"
+satyMevJayteLogoImg.width = "100"
+satyMevJayteLogoImg.height = "100"
+satyMevJayteLogoImg.style.objectFit = "contain"
+
+//create second div
+const governmentDiv = document.createElement("div")
+governmentDiv.style.width = "80%"
+governmentDiv.className = "border-start border-dark d-flex gap-3 ps-2 align-items-center"
+
+
+//create a div for logo of cet
+const cetLogo = document.createElement("div")
+cetLogo.style.width = "100px"
+cetLogo.style.height = "100px"
+
+//create img tag to cet logo
+const cetLogoImg = document.createElement("img")
+cetLogoImg.src = "../img/cet_cell-removebg-preview.png"
+cetLogoImg.alt = "cet_cell-removebg-preview"
+cetLogoImg.width = "100"
+cetLogoImg.height = "100"
+cetLogoImg.style.objectFit = "contain"
+
+//create a div for givernment section
+const governmentDivContent = document.createElement("div")
+governmentDivContent.style.flex = 1
+
+
+//create a heading to governmentDivContent
+const governmentDivContentH1 = document.createElement("h1")
+governmentDivContentH1.innerText = "GOVERNMENT OF MAHARASHTRA"
+governmentDivContentH1.className = "fw-bold"
+governmentDivContentH1.style.fontSize = "20px"
+
+//create a heading2 to governmentDivContent
+const governmentDivContentH5 = document.createElement("h5")
+governmentDivContentH5.innerText = "State Common Entrance Test Cell"
+governmentDivContentH5.className = "fw-bold"
+governmentDivContentH5.style.fontSize = "15px"
+
+//create a government logo
+const governmentLogo = document.createElement("div")
+governmentLogo.style.width = "100px"
+governmentLogo.style.height = "100px"
+
+
+//create img tag to government logo
+const governmentLogoImg = document.createElement("img")
+governmentLogoImg.src = "../img/Seal_of_Maharashtra.svg.png"
+governmentLogoImg.alt = ""
+governmentLogoImg.width = "100"
+governmentLogoImg.height = "100"
+governmentLogoImg.style.objectFit = "contain"
+
+governmentLogo.appendChild(governmentLogoImg)
+cetLogo.appendChild(cetLogoImg)
+governmentDiv.appendChild(cetLogo)
+governmentDiv.appendChild(governmentDivContent)
+governmentDivContent.appendChild(governmentDivContentH1)
+governmentDivContent.appendChild(governmentDivContentH5)
+satyMevJayteLogo.appendChild(satyMevJayteLogoImg)
+headerMiddlerWrapper.appendChild(satyMevJayteLogo)
+headerMiddlerWrapper.appendChild(governmentDiv)
+headerMiddlerWrapper.appendChild(governmentLogo)
+headerMiddle.appendChild(headerMiddlerWrapper)
+header.appendChild(headerMiddle)
 
 
 
